490bb5cbc537d3d47bfc2709e5bcba6ea7581539
bharathmyadagoni <135199905+bharathmyadagoni@users.noreply.github.com>
2023-12-13 15:46:35 +0530

    Add files via upload

---
+++ Web Page/index.js
@@ -0,0 +1,165 @@
+let dtn1 = document.getElementById("btn1")
+let dtn2 = document.getElementById("btn2")
+let dtn3 = document.getElementById("btn3")
+let dtn4 = document.getElementById("btn4")
+let dtn5 = document.getElementById("btn5")
+let con2 = document.getElementById("con2")
+let img1 = document.getElementById("im1")
+let img2 = document.getElementById("im2")
+let img3 = document.getElementById("im3")
+let img4 = document.getElementById("im4")
+let img5 = document.getElementById("im5")
+let p5 = document.getElementById("p5")
+let p1 = document.getElementById("p1")
+let p2 = document.getElementById("p2")
+let p3 = document.getElementById("p3")
+let p4 = document.getElementById("p4")
+
+p1.style.backgroundColor = "white";
+p2.style.backgroundColor = "white";
+p3.style.backgroundColor = "white";
+p4.style.backgroundColor = "white";
+p5.style.backgroundColor = "white";
+
+
+
+dtn1.onclick = function () {
+    con2.style.fontFamily = "roboto";
+    dtn1.style.backgroundColor = "#0080ff"
+    dtn2.style.backgroundColor = "#dcdcdc"
+    dtn3.style.backgroundColor = "#dcdcdc"
+    dtn4.style.backgroundColor = "#dcdcdc"
+    dtn5.style.backgroundColor = "#dcdcdc";
+
+    p1.style.backgroundColor = "#87cfeb";
+    p2.style.backgroundColor = "white"
+    p3.style.backgroundColor = "white"
+    p4.style.backgroundColor = "white"
+    p5.style.backgroundColor = "white";
+
+    p1.style.width = "120px"
+    // document.getElementById("p2").style.width = "none"
+    // document.getElementById("p3").style.width = "none"
+    // document.getElementById("p4").style.width = "none"
+    // document.getElementById("p5").style.width = "none"
+    img1.style.display = "block"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+
+
+
+}
+dtn2.onclick = function () {
+    con2.style.fontFamily = "sans-serif";
+    dtn2.style.backgroundColor = "#0080ff"
+    dtn1.style.backgroundColor = "#dcdcdc"
+    dtn3.style.backgroundColor = "#dcdcdc"
+    dtn4.style.backgroundColor = "#dcdcdc"
+    dtn5.style.backgroundColor = "#dcdcdc"
+    p2.style.backgroundColor = "#87cfeb"
+    p1.style.backgroundColor = "white"
+    p3.style.backgroundColor = "white"
+    p4.style.backgroundColor = "white"
+    p5.style.backgroundColor = "white"
+    p2.style.width = "120px"
+    // document.getElementById("p1").style.width = "none"
+    // document.getElementById("p3").style.width = "none"
+    // document.getElementById("p4").style.width = "none"
+    // document.getElementById("p5").style.width = "none"
+    img2.style.display = "block"
+    img1.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+
+
+
+
+
+
+
+}
+dtn3.onclick = function () {
+    con2.style.fontFamily = "Georgia";
+    dtn3.style.backgroundColor = "#0080ff"
+    dtn1.style.backgroundColor = "#dcdcdc"
+    dtn2.style.backgroundColor = "#dcdcdc"
+    dtn4.style.backgroundColor = "#dcdcdc"
+    dtn5.style.backgroundColor = "#dcdcdc"
+    p3.style.backgroundColor = "#87cfeb"
+    p1.style.backgroundColor = "white"
+    p2.style.backgroundColor = "white"
+    p5.style.backgroundColor = "white"
+    p4.style.backgroundColor = "white"
+    p3.style.width = "120px"
+    // document.getElementById("p2").style.width = "none"
+    // document.getElementById("p1").style.width = "none"
+    // document.getElementById("p4").style.width = "none"
+    // document.getElementById("p5").style.width = "none"
+    img3.style.display = "block"
+    img2.style.display = "none"
+    img1.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+
+
+
+
+
+
+}
+dtn4.onclick = function () {
+    con2.style.fontFamily = "Courier new";
+    dtn4.style.backgroundColor = "#0080ff"
+    dtn1.style.backgroundColor = "#dcdcdc"
+    dtn3.style.backgroundColor = "#dcdcdc"
+    dtn2.style.backgroundColor = "#dcdcdc"
+    dtn5.style.backgroundColor = "#dcdcdc"
+    p4.style.backgroundColor = "#87cfeb"
+    p1.style.backgroundColor = "white"
+    p2.style.backgroundColor = "white"
+    p3.style.backgroundColor = "white"
+    p5.style.backgroundColor = "white"
+    p4.style.width = "120px"
+    // document.getElementById("p2").style.width = "none"
+    // document.getElementById("p3").style.width = "none"
+    // document.getElementById("p1").style.width = "none"
+    // document.getElementById("p5").style.width = "none"
+    img4.style.display = "block"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img1.style.display = "none"
+    img5.style.display = "none"
+
+
+
+}
+dtn5.onclick = function () {
+    con2.style.fontFamily = "monospace";
+    dtn5.style.backgroundColor = "#0080ff"
+    dtn1.style.backgroundColor = "#dcdcdc"
+    dtn3.style.backgroundColor = "#dcdcdc"
+    dtn4.style.backgroundColor = "#dcdcdc"
+    dtn2.style.backgroundColor = "#dcdcdc"
+    p5.style.backgroundColor = "#87cfeb"
+    p1.style.backgroundColor = "white"
+    p2.style.backgroundColor = "white"
+    p3.style.backgroundColor = "white"
+    p4.style.backgroundColor = "white"
+    p5.style.width = "120px"
+    // document.getElementById("p2").style.width = "none"
+    // document.getElementById("p3").style.width = "none"
+    // document.getElementById("p4").style.width = "none"
+    // document.getElementById("p1").style.width = "none"
+    img5.style.display = "block"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img1.style.display = "none"
+
+
+
+
+}
